44e2f41ab712f87984a155e7b2175b65ac507654
Albin Håkanson <33166817+Tooxic@users.noreply.github.com>
2021-08-24 16:22:05 +0200

    something

---
+++ Miniräknare/Miniräknare/Miniräknare/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Miniräknare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.DarkBlue;
+            Console.Beep();
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
