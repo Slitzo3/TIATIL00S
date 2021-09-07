3c6e1e324a323233df030f770494b2b4431c678b
Albin Håkanson <33166817+Tooxic@users.noreply.github.com>
2021-09-07 15:39:51 +0200

    Big commit

---
+++ Basics/Basics/Basics/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Basics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hej");
+            //Console.WriteLine("Jag heter");
+            //Console.WriteLine("TIA");
+
+            //Variabler
+            int i = 1;
+
+
+            int age;
+            double weight;
+            string name;
+            bool IsStudent = true; // or false
+
+            Console.WriteLine("Hi, Welcome to Siri \nWhats your name?");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Whats your age?");
+            age = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Whats your weight?");
+            weight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Hi my name is " + name + "\n And my age is " + age);
+            Console.WriteLine("My weight is " + weight + " kg");
+            if(IsStudent)
+            {
+                Console.WriteLine("And i'm a student at NTI Kronhus");
+            }
+            else
+            {
+                Console.WriteLine("När detta blir falskt");
+            }
+        }
+    }
+}
---
+++ IfElseAndLoop/IfElseAndLoop/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace IfElseAndLoop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Skrin in ditt namn"); //skriv in ditt namn
+            //string namn = Console.ReadLine();
+
+
+            //Console.WriteLine("Här tar jag slut");
+
+            double lektiontid = 15.00d;
+            double lararenAnkomst;
+            Console.WriteLine("När kom läraren?");
+            lararenAnkomst = double.Parse(Console.ReadLine());
+
+            if(lararenAnkomst > (lektiontid + 0.15d))
+            { 
+                Console.WriteLine("Då får du gå");
+            }
+            else
+            {
+                Console.WriteLine("Yes vi har lektion");
+            }
+
+
+        }
+    }
+}
